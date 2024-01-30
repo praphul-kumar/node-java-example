26db2fab81408c51c3ce0dd6f5690b4a492b2c0a
Praphul Kumar <praphulkumar2023@gmail.com>
2024-01-30 12:03:16 +0530

    Basic Aritmetic operations in java

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ const java = require('java');
 const javalangSystem = java.import('java.lang.System');
 
 javalangSystem.out.printlnSync('Hello World!');
+
+console.log('Basic Arithmetic Operation in JAVA.');
+
+const a = 10;
+const b = 20;
+
+javalangSystem.out.printlnSync(a + b);
+javalangSystem.out.printlnSync(a - b);
+javalangSystem.out.printlnSync(a * b);
+javalangSystem.out.printlnSync(b / a);
