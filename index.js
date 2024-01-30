482f99f98a8cc9bc2bd9095058a6905ed0ac3d38
Praphul Kumar <praphulkumar2023@gmail.com>
2024-01-30 15:10:03 +0530

    Using Java Class inside NodeJs Project

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
+const path = require('path');
 const java = require('java');
-const javalangSystem = java.import('java.lang.System');
 
-javalangSystem.out.printlnSync('Hello World!');
+// Adding jar file path into classpath
+const jarPath = path.join(__dirname, 'assets/', 'Dummy.jar');
+java.classpath.push(jarPath);
 
-console.log('Basic Arithmetic Operation in JAVA.');
+// importing Java Class into NodeJS
+const Dummy = java.import('com.example.Dummy');
 
-const a = 10;
-const b = 20;
+// Calling static method hello() from Java Class Dummy.
+Dummy.hello("Hello World");
 
-javalangSystem.out.printlnSync(a + b);
-javalangSystem.out.printlnSync(a - b);
-javalangSystem.out.printlnSync(a * b);
-javalangSystem.out.printlnSync(b / a);
+// Calling static method sum Synchronously 
+console.log(Dummy.sumSync(1, 3));
+
+// Creating an object of Dummy Class
+const dummy1 = new Dummy();
+
+// calling instance method of Java class Dummy in NodeJS
+const greet = dummy1.grettingsSync('Praphul');
+
+console.log(greet);
